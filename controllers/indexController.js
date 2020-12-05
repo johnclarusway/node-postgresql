a102fe958a1a9c22165f69303821e2c7d1de0e91
ms <ms@gmail.com>
2020-12-05 19:44:16 +0200

    Add controllers

---
+++ controllers/indexController.js
@@ -0,0 +1,6 @@
+exports.get_index = function (req, res, next) {
+  //job1
+  //job2
+  //job3
+  res.render("index", { title: "Express" });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const usersController = require("../controllers/usersController");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/*  /user */
+
+router.get("/", usersController.get_users);
 
 module.exports = router;
