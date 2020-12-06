a8e5f2a699c7a0da02bdbc8930360cadbc199542
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-07 00:52:20 +0300

    added get&post routers for edit user

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,7 @@ router.post("/add", usersController.add_user);
 
 router.get("/:id/delete", usersController.delete_user);
 
+router.get("/:id/:firstName/:lastName/edit", usersController.show_edit_user_form);
+router.post("/:id/edit", usersController.edit_user);
+
 module.exports = router;
